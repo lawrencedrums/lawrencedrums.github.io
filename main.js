07d30e6fdfc761e18caa854b6385e0347b8e4302
Lawrece Wong <51978449+lawrencedrums@users.noreply.github.com>
2020-07-03 12:00:31 +0800

    Implemented bubblesort and refactor
    Include a sort button that only does bubblesort for now.
    
    Disabled time delay when generating new array because it's interfering with async functions.
    
    Refactored the array generating functions.

--- main.js
+++ main.js
@@ -1,35 +1,90 @@
-let arraySize = 100
-let canvas = document.getElementById('mainCanvas');
-let c = canvas.getContext('2d');
+let array, arraySize, canvas, ctx, delayTime, rect, num;
 
-// Make canvas as large as the user's screen
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function init() {
+    arraySize = 100;
+    // delayTime = 10;
+    canvas = document.getElementById('mainCanvas');
+    ctx = canvas.getContext('2d');
 
-function getNewArr() {
-    // Poplate an array of 1 to arraySize
-    let array = [];
+    // Make canvas as large as the user's screen
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Populate new array
+    array = [];
     for(let i = 1; i <= arraySize; i++) {
         array.push(i);
     }
 
+    randomArr();
+}
+
+function randomArr() {
+    //disableButtons(delayTime * arraySize);
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     shuffle(array);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw new array each a delay between each rectangle
+    //setTimeout(() => {
+    drawArr(array); 
+    //}, delayTime * i);
+}
+
+// Create each rectangles with colour and length based on the number size in array
+function drawArr(array) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let i = 0; i < arraySize; i++) { 
-        drawArr(i, array); 
+        ctx.fillStyle = getHSL(array[i]);
+        rect = getPos(i, array[i]);
+        ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
     }
 }
 
-// Create rectangles with colour and length based the number size in array
-// Each with a set time delay
-function drawArr(i, array) {
-    setTimeout(function() {
-        c.fillStyle = getHSL(array[i]);
-        let rect = getPos(i, array[i]);
-        c.fillRect(rect.x, rect.y, rect.width, rect.height);
-    }, 3 * i);
+async function bubbleSort() {
+    let n, i, j;
+    let end = arraySize;
+
+    document.getElementById("randombtn").disabled = true;
+    document.getElementById("sortbtn").disabled = true;
+
+    for (n = 0; n < end; end--) {
+        for (i = 0, j = 1; i < arraySize; i++, j++) {
+            if (array[i] > array[j]) {
+                await swap(array, i, j);
+            }
+            drawArr(array, i);
+        }
+    }
+    document.getElementById("randombtn").disabled = false;
+    document.getElementById("sortbtn").disabled = false;
 }
+
+async function swap(array, x, y) {
+    await sleep(5);
+    let temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+}
+
+// Resolve a promise after a set-time
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Disable and re-enable buttons after a set-time delay
+// function disableButtons(time) {
+//     document.getElementById("randombtn").disabled = true;
+//     document.getElementById("sortbtn").disabled = true;
+
+//     setTimeout(() => {
+//         document.getElementById("randombtn").disabled = false;
+//         document.getElementById("sortbtn").disabled = false;
+//     }, time);
+// }
 
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
@@ -62,10 +117,10 @@ function getHSL(i) {
 // Find the position of where each rectangle should start
 function getPos(i, num) {
     let rectangle = {
-        x: 10 + i * (canvas.width - 20) / arraySize,
-        y: canvas.height - 10,
+        x: 20 + i * (canvas.width - 40) / arraySize,
+        y: canvas.height - 20,
         width: (canvas.width - 20) / arraySize - 4,
-        height: ((canvas.height - 50) / arraySize) * -num
+        height: ((canvas.height - 40) / arraySize) * -num
     };
     return rectangle;
 }
