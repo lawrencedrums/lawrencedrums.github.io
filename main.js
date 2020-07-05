81944c86f9857af2514c25d325d1b2f3ef5ba9ac
Lawrece Wong <51978449+lawrencedrums@users.noreply.github.com>
2020-07-05 20:15:48 +0800

    Drop-down menu for algorithms. Draw style changes.
    Version 1 as stated in README.md
    
    Implemented a new shuffle algorithm that looks prettier because it looks more "random". Implemented a drop-down menu for choosing algorithms.
    
    Found a bug when using on the live version, the array won't show on mobile. Stackoverflow said it might be a "let" vs "var" problem. Will experiment with that option next version.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-let array, arraySize, canvas, ctx, delayTime, rect, num;
+let array, canvas, ctx, rect, num;
+let arraySize = 100;
+let delayTime = 8;
 
 function init() {
-    arraySize = 100;
-    // delayTime = 10;
+    disableButtons();
     canvas = document.getElementById('mainCanvas');
     ctx = canvas.getContext('2d');
 
@@ -12,33 +13,39 @@ function init() {
 
     // Populate new array
     array = [];
-    for(let i = 1; i <= arraySize; i++) {
+    for (let i = 1; i <= arraySize; i++) {
         array.push(i);
     }
 
-    randomArr();
+    drawArr(array);
+
+    // Randomize after a set delay
+    setTimeout(() => {
+        randomizeArray(array);
+    }, 1000);
 }
 
-function randomArr() {
-    //disableButtons(delayTime * arraySize);
+async function randomizeArray() {
+    disableButtons();
 
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    shuffle(array);
-
-    // Draw new array each a delay between each rectangle
-    //setTimeout(() => {
-    drawArr(array); 
-    //}, delayTime * i);
+    await prettierShuffle(array);
+    enableButtons();
 }
 
-// Create each rectangles with colour and length based on the number size in array
-function drawArr(array) {
+// Draw rectangles in array instantly. Colour rectangle black if that number is being sorted.
+function drawArr(array, beingSorted) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let i = 0; i < arraySize; i++) { 
-        ctx.fillStyle = getHSL(array[i]);
+
+        if (i == beingSorted) {
+            ctx.fillStyle = "hsl(0, 0%, 0%)";
+        } else {
+            ctx.fillStyle = getHSL(array[i]);
+        }
         rect = getPos(i, array[i]);
         ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
     }
@@ -47,74 +54,97 @@ function drawArr(array) {
 async function bubbleSort() {
     let n, i, j;
     let end = arraySize;
-
-    document.getElementById("randombtn").disabled = true;
-    document.getElementById("sortbtn").disabled = true;
+    disableButtons();
 
     for (n = 0; n < end; end--) {
         for (i = 0, j = 1; i < arraySize; i++, j++) {
             if (array[i] > array[j]) {
                 await swap(array, i, j);
+                drawArr(array, j);
             }
-            drawArr(array, i);
         }
     }
-    document.getElementById("randombtn").disabled = false;
-    document.getElementById("sortbtn").disabled = false;
+    drawArr(array, j);
+    enableButtons();
+}
+
+async function insertionSort() {
+    let key, i, j;
+    disableButtons();
+
+    for (i = 1; i < arraySize; i++) {
+        key = array[i];
+        j = i - 1;
+
+        while (j >= 0 && array[j] > key) {
+            await sleep(delayTime);
+            array[j + 1] = array[j];
+            drawArr(array, j);
+            j--;
+        }
+        array[j + 1] = key;
+    }
+    drawArr(array);
+    enableButtons();
 }
 
 async function swap(array, x, y) {
-    await sleep(5);
+    await sleep(delayTime);
     let temp = array[x];
     array[x] = array[y];
     array[y] = temp;
 }
 
-// Resolve a promise after a set-time
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Disable and re-enable buttons after a set-time delay
-// function disableButtons(time) {
-//     document.getElementById("randombtn").disabled = true;
-//     document.getElementById("sortbtn").disabled = true;
+function disableButtons() {
+    document.getElementById("randombtn").disabled = true;
+    document.getElementById("sorts").disabled = true;
+}
 
-//     setTimeout(() => {
-//         document.getElementById("randombtn").disabled = false;
-//         document.getElementById("sortbtn").disabled = false;
-//     }, time);
-// }
+function enableButtons() {
+    document.getElementById("randombtn").disabled = false;
+    document.getElementById("sorts").disabled = false;
+}
 
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-    
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+// A shuffle that looks prettier and more "random"
+async function prettierShuffle(array) {
+    let shuffledNumber = [], randomIndex, randomIndexTwo;
+
+    // As long as there's unshuffled index left...
+    while (shuffledNumber.length !== arraySize) {
+
+        // Randomly get 2 indices
+        randomIndex = Math.floor(Math.random() * arraySize);
+        randomIndexTwo = Math.floor(Math.random() * arraySize);
+
+        // If the random indices are different...
+        if (randomIndex !== randomIndexTwo && 
+
+            // And indices are not shuffled...
+            shuffledNumber.includes(randomIndex) == false && shuffledNumber.includes(randomIndexTwo) == false) {
+
+            // Swap them and push them into shuffled list of numbers
+            await swap(array, randomIndex, randomIndexTwo);
+            shuffledNumber.push(randomIndex, randomIndexTwo)
+            drawArr(array);
+        }
     }
 }
 
-// Find HSL Hue value based on number size in array
+// Calculate HSL Hue value based on number size in array
 function getHSL(i) {
     if (i === 0 || i === arraySize) {
         return "hsl(0, 100%, 50%)";
-    }
-    else {
+    } else {
         let hue = 360 / arraySize * i;
         return "hsl(" + hue + ", 100%, 50%)";
     }
 }
 
-// Find the position of where each rectangle should start
+// Find the position and height of each rectangle
 function getPos(i, num) {
     let rectangle = {
         x: 20 + i * (canvas.width - 40) / arraySize,
@@ -124,3 +154,20 @@ function getPos(i, num) {
     };
     return rectangle;
 }
+
+// Fisher–Yates shuffle algorithm referenced from stackoverflow
+// async function shuffle(array) {
+//     let currentIndex = arraySize, randomIndex;
+  
+//     // While there remain elements to shuffle...
+//     while (0 !== currentIndex) {
+  
+//         // Pick a remaining element...
+//         randomIndex = Math.floor(Math.random() * currentIndex);
+//         currentIndex -= 1;
+    
+//         // And swap it with the current element.
+//         await swap(array, currentIndex, randomIndex);
+//         drawArr(array);
+//     }
+// }
